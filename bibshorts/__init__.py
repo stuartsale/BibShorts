8474e5daa4bbd3eb25911cb2ae0603e5076efaad
Stuart Sale <stuart.sale@gmail.com>
2017-01-15 15:25:26 +0000

    Add use of bibtexparser package
    Use the bibtexparser package to parse a bibtex entry into a python
    dict.

--- bibshorts/__init__.py
+++ bibshorts/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import bibtexparser as bp
 import hashlib
 import random
 import re            # regular expresions
@@ -26,24 +27,25 @@ FORMAT_BIBTEX = 4
 class bib_entry:
 
     def __init__(self, bibtex_in):
-        """ 
+        """
         Initialise the object with its arxiv reference number
         """
         self.bibtex = bibtex_in
+        self.bibtex_dict = bp.loads(self.bibtex).entries_dict.values()[0]
+        self.bibtype = self.bibtex_dict['ENTRYTYPE']
+
+        self.search_success = False
 
     def __cmp__(self, other):
         return cmp(self.key, other.key)
 
-
     def get_dx_doi(self):
-        """ 
+        """
         Scrape full bibtex off dx.doi.org if a doi is contained in entry
         """
 
-        self.search_success = False
-
         doi_pattern = re.compile('doi = .*$', re.MULTILINE)
-        
+
         try:
             doi = doi_pattern.search(self.bibtex).group().lstrip("doi = {").\
                     rstrip("},")
@@ -56,9 +58,10 @@ class bib_entry:
 
         url_headers = {"Accept": "application/x-bibtex"}
         r = requests.get(url, headers=url_headers)
-        self.bibtex = r.text
+        bibtex = r.text
         self.search_sucess = True
 
+        return bibtex
 
     def get_google_doi(self):
         """
@@ -68,7 +71,7 @@ class bib_entry:
         self.search_success = False
 
         doi_pattern = re.compile('doi = .*$', re.MULTILINE)
-        
+
         try:
             doi = doi_pattern.search(self.bibtex).group().lstrip("doi = {").\
                     rstrip("},")
@@ -89,77 +92,69 @@ class bib_entry:
 
         r = requests.get(bibtex_url)
         bibtex = r.text
-        bibtex.replace("=", " = ")
-        self.bibtex = bibtex
+        bibtex = bibtex.replace("=", " = ")
         self.search_success = True
-        
 
+        return bibtex
 
     def set_key(self):
-        """ 
+        """
         Alter the Bibtex entry's key to match prefered scheme
         """
 
         replace_list = ["{", "\\", "}", "'", "`", '"', "\n", "\t", "^", " "]
 
+        try:
+            author_list = self.bibtex_dict['author'].split(' and ')
+        except:
+            author_pattern = re.compile('author = .*\.}\,$|author = .*\t}\,$',
+                                        re.MULTILINE | re.DOTALL)
+            author_list = (author_pattern.search(self.bibtex).group().
+                           lstrip("author = ").rstrip("},")[1:])
+            author_list = author_list.split(' and ')
 
-        author_pattern = re.compile('author = .*\.}\,$|author = .*\t}\,$', 
-                                    re.MULTILINE | re.DOTALL)
-        author_list = author_pattern.search(self.bibtex).group().\
-                        lstrip("author = ").rstrip("},")[1:]
-        author_list = author_list.split(' and ')
-
-
-        if len(author_list)==0:
-            print("ERROR")
+        if len(author_list) == 0:
+            raise AttributeError("No author names found")
             return
 
         name_pattern = re.compile('{[^,]*}')
-        name1 = name_pattern.search(author_list[0]).group().lstrip("{").rstrip("}")
-        name1 = name1.replace("{","").replace("\\","").replace("}","").\
-                      replace("'","").replace("`","").replace('"',"").\
-                      replace("\n","").replace("\t","").replace("^","").\
-                      replace(" ","").strip(" ")
+        name1 = (name_pattern.search(author_list[0]).group().lstrip("{")
+                 .rstrip("}"))
+        name1 = (name1.replace("{", "").replace("\\", "").replace("}", "").
+                 replace("'", "").replace("`", "").replace('"', "").
+                 replace("\n", "").replace("\t", "").replace("^", "").
+                 replace(" ", "").strip(" "))
 
-
-        if len(author_list)==1:
+        if len(author_list) == 1:
             name2 = "only"
 
-        elif len(author_list)>1:
+        elif len(author_list) > 1:
             try:
                 name2 = name_pattern.search(author_list[1]).group().\
                                 lstrip("{").rstrip("}")
-                name2 = name2.replace("{","").replace("\\","").\
-                              replace("}","").replace("'","").replace("`","").\
-                              replace('"',"").replace("\n","").\
-                              replace("\t","").replace("^","").\
-                              replace(" ","").strip(" ")
+                name2 = (name2.replace("{", "").replace("\\", "").
+                         replace("}", "").replace("'", "").replace("`", "").
+                         replace('"', "").replace("\n", "").replace("\t", "").
+                         replace("^", "").replace(" ", "").strip(" "))
             except AttributeError:
                 name2 = "only"
 
-        
         year_pattern = re.compile('year = .*$', re.MULTILINE)
-        year = year_pattern.search(self.bibtex).group().lstrip('year =').\
-                        rstrip(",")
+        year = (year_pattern.search(self.bibtex).group().lstrip('year =').
+                rstrip(","))
 
         self.key = name1+"_"+name2+"."+year
         print(self.key)
 
-
-
-    def bibtex_write(self, output_file):
+    def bibtex_write(self, output_obj):
         """
-        Dump bibtex to file, checking key not already used
+        Dump bibtex to file.
         """
-        for other_key in written_keys:
-            if other_key==self.key:
-                self.key = self.key+"a"
 
-            written_keys.append(self.key)
+        output_obj.write(self.bib_type+"{"+self.key+",\n")
 
-        split_bibtex = self.bibtex.split("\n")
-
-        output_file.write(self.bib_type+"{"+self.key+",\n")
-
-        for n in range(1, len(split_bibtex)):
-            output_file.write(split_bibtex[n]+"\n")
+        for field in self.bibtex_dict:
+            if field is not "ENTRYTYPE":
+                output_obj.write(" {0} = \{{1}\},\n".format(
+                            field, self.bbibtex_dict[field]))
+        output_obj.write("}\n")
